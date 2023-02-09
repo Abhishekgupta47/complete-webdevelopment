6c65f9784f6e05313259ae95fa32e66005090607
Abhishek Gupta <92316753+Abhishekgupta47@users.noreply.github.com>
2023-02-09 23:34:23 +0530

    daily-study

---
+++ JAVASCRIPT/tut53.js
@@ -0,0 +1,24 @@
+console.log("this is tutorial 53");
+let name="abhi";
+let name1="shubh";
+let name2="aryan";
+let name3="anmol";
+let greetText="Good Morning";
+let returnVal = sum(1,2,3);
+  
+
+function greet(name , greetText="i cant say anything about him"){
+    // console.log(greetText + " "+name)
+    console.log(greetText+" "+name)
+   console.log( name+ " is a goog boy");
+}
+function sum(a,b,c){
+    let d = a+b+c;
+    return d;
+}
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, );
+greet(name3, greetText);
+// returnval
+console.log(returnVal);
